631684efe0108857343e14045aaca8f7d10ae98e
Sten Ehrberg <sten.ehrberg@gmail.com>
2020-12-15 23:25:33 +0200

    some temp styling for trick page, temp gitignore for wwwwroot (local video uploads)

--- src/web-client/pages/tricks/_trick.vue
+++ src/web-client/pages/tricks/_trick.vue
@@ -1,17 +1,19 @@
 <template>
-    <div>
-        <div>Trick: {{ $route.params.trick }}</div>
-        <div v-if="submissions">
-            <div :key="indx" v-for="(s, indx) in submissions">
-                {{ s.id }} - {{ s.description }} trickId: {{ s.trickId }}
-                <div>
-                    <video
-                        controls
-                        :src="`http://localhost:5000/api/videos/${s.video}`"
-                    ></video>
+    <div class="d-flex justify-center align-start">
+        <div class="mx-5 mt-8" v-if="submissions">
+            <div :key="x" v-for="x in 20">
+                <div :key="indx" v-for="(s, indx) in submissions">
+                    {{ s.id }} - {{ s.description }} trickId: {{ s.trickId }}
+                    <div>
+                        <video
+                            controls
+                            :src="`http://localhost:5000/api/videos/${s.video}`"
+                        ></video>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="mx-5 mt-8 sticky">Trick: {{ $route.params.trick }}</div>
     </div>
 </template>
 
@@ -19,7 +21,6 @@
 import { mapState } from "vuex";
 
 export default {
-
     computed: mapState("submissions", ["submissions"]),
 
     async fetch() {
@@ -32,3 +33,11 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.sticky {
+    position: sticky;
+    position: -webkit-sticky;
+    top: 64px;
+}
+</style>
